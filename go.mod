1eb26bf3f8ce2c0f511c73dbdc9a928bd105e7a4
leslie-fei <andyl@dianhun.cn>
2024-05-14 16:42:23 +0800

    perf: Utilizing a more efficient hash algorithm

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module memlru
 go 1.21
 
 require golang.org/x/sys v0.20.0
+
+require github.com/dolthub/maphash v0.1.0
--- utils.go
+++ utils.go
@@ -11,14 +11,14 @@ func RangeNode(base uintptr, offset uint64, f func(node *DataNode) bool) {
 	if offset == 0 {
 		return
 	}
-	for node := ToLinkedNode(base, offset); node != nil; node = ToLinkedNode(base, offset) {
+	for node := ToDataNode(base, offset); node != nil; node = ToDataNode(base, offset) {
 		if !f(node) {
 			return
 		}
 	}
 }
 
-func ToLinkedNode(base uintptr, offset uint64) *DataNode {
+func ToDataNode(base uintptr, offset uint64) *DataNode {
 	if offset == 0 {
 		return nil
 	}
